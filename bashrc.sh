746735126d9f39496b11a193f9348e94ef89d91b
y0zg <webstd.design@gmail.com>
2019-03-19 18:50:30 +0200

    Create bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source <(kubectl completion bash)
+complete -F __start_kubectl k
+
+alias k=”kubectl”
+alias kgd=”k get deploy”
+alias kgp=”k get pods”
+alias kgn=”k get nodes”
+alias kgs=”k get svc” 
+alias kge=”k get events — sort-by=’.metadata.creationTimestamp’ |tail -8"
+
+export nks=”-n kube-system” 
+export ETCDCTL_API=3
+export k8s=”https://k8s.io/examples"
+
+function vaml()
+{
+ vim -R -c ‘set syntax=yaml’ -; 
+}
+
