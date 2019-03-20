43de3e92b0ba5b13f4f9b23a0fa0b1cefdb4709a
teevi <webstd.design@gmail.com>
2019-03-21 00:13:57 +0200

    Helm chart

--- kubia/app.js
+++ kubia/app.js
@@ -2,9 +2,9 @@ const http = require('http');
 const os = require('os');
 console.log("Kubia server starting...");
 var handler = function(request, response) {
- console.log("Received request from " + request.connection.remoteAddress);
+ console.log("Received request from HOST:" + request.connection.remoteAddress);
  response.writeHead(200);
- response.end("What's up? You've hit " + os.hostname() + "\n");
+ response.end("New images. What's up? You've hit " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8081);
